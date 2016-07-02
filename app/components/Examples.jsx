31404eb9583fa67589b585d7ee43ffcd6114a19e
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-07-02 18:45:22 +0100

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,17 +1,12 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-// render:function() {
-//   return(
-//     <h1>This Is example page</h1>
-//     );
-// }
-// });
-
-
 var Examples = (props) =>{
   return(
+
+    <div>
     <h3>This Is example page</h3>
+    <p>Welcome</p>
+    </div>
     )
 };
 
