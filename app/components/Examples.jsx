8891b9d7c3ebf23d83c64e608195a74a7ff79475
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-07-14 14:51:08 +0100

    add gride example and style exmaples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) =>{
   return(
 
     <div>
-    <h3>This Is example page</h3>
-    <p>Welcome</p>
+    <h1 className="text-center">Examples</h1>
+    <p>here are few exmaples you can try out</p>
+    <ol>
+      <li>
+        <Link to='/?location=london'>uk, london</Link>
+      </li>
+      <li>
+        <Link to='/?location=sweden'>Sweden, swe</Link>
+      </li>
+    </ol>
     </div>
     )
 };
