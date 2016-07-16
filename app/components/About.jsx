8ce5b0889e5b523375cbec19992a941eaa37924e
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-07-16 19:43:57 +0100

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,21 @@ var React = require('react');
 var About = (props) =>{
 
   return(
-        <h3>About Component</h3>
-      )
+    <div>
+      <h1 className ="text-center">About Component</h1>
+      <p>This is a weather application build on React.</p>
+      <p>Here some Tools that i used</p>
+      <ul>
+        <li>
+          <a herf="https://facebook.github.io/react">React</a> - this was the JavaScript framework i used
+        </li>
+        <li>
+          <a herf="https://home.openweathermap.org">React</a> - Weather API
+        </li>
+      </ul>
+    </div>
+
+      );
 };
 
 module.exports = About;
